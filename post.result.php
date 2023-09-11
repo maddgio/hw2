0b0a11acc0cbab9e456c2a602c44df52eb342519
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-09-11 14:54:14 -0500

    Create post.result.php
    need to go back and complete this - 30:17 of video

---
+++ post.result.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = "Post Result";
+include "view-header.php";
+?>
+    <h1>Post Resutls</h1>
+<?php  
+indluce (asset($_POST<form method="post" action="post-result.php">
+  <input type = "text" name="my-name">
+  <input type="submit" value="Post Data">
+</form>
+    <?php
+include "view-footer.php";
+?
