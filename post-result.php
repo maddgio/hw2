5440dfbec479c2383d68ddab0bec7a8861877e29
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-09-17 13:33:08 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,15 +4,14 @@ include "view-header.php";
 ?>
     <h1>Post Resutls</h1>
 <?php  
-echo getdisplay(); 
-include "view-dooter.php";
+echo getDisplay(); 
+include "view-footer.php";
 
-function getdisplay() {
+function getDisplay() {
     if (isset($_POST['my-name'])) {
-        return "<p> The value sent is: <p>" echo $_POST['my-name"];
+        return "<p> The value sent is: <p>" . $_POST['my-name"];
 } else {
 return "<p> Nothing posted to the page.</p>";
-<?php
 } 
 }
     ?>
