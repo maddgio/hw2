b8c27323be3d3761f2290c0bd6555032b0b49b90
Madison Giovenco <113938827+maddgio@users.noreply.github.com>
2023-09-17 13:40:31 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -11,7 +11,7 @@ function getDisplay() {
     if (isset($_POST['my-name'])) {
         return "<p> The value sent is: <p>" . $_POST['my-name"];
 } else {
-return "<p> Nothing posted to the page.</p>";
+   return "<p> Nothing posted to the page.</p>";
 } 
 }
     ?>
